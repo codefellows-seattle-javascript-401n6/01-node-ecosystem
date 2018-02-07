1ec59502577cb67b7f1600343c7dd0eee68d5474
ronnie1734 <rlewwwis@yahoo.com>
2018-02-06 20:05:40 -0800

    first test working

---
+++ test/greet.test.js
@@ -0,0 +1,18 @@
+const greet = require('../lib/greet').greet;
+
+describe('greet',() => {
+    it('should return null with non string values', () => {
+        let str = 3;
+        let result = greet(str);
+        let expected = null;
+        expect(result).toBe(expected);
+    })
+
+    it('should return hello world when world is supplied', () => {
+        let str = 'world';
+        let result = greet(str);
+        let expected = 'hello world';
+        expect(result).toBe(expected);
+    })
+
+})
