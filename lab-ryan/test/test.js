2cb70b0fcbc87c9b5ba514eb33cf08e9397c0ff2
rjtj2007 <teacherman1@gmail.com>
2018-02-06 20:41:36 -0800

    finished greet and arithmetic

---
+++ lab-ryan/test/test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const greet = require('.lib/greet.js').greet;
+
+describe("greet function", () => {
+    it("it should return null with non-string value")
+})
+
+// Write a test that expects the greet module to return null when you supply non string values
+// Write a test the expects the greet module to return 'hello world'
+// This should happen when invoked with 'world' as the first argument
