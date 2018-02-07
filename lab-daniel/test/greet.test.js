0ed49d73ed74c3f6da1da51264c08eaf69791774
daniel <dpillay03@gmail.com>
2018-02-06 21:25:15 -0800

    greet.js is passing all of its tests!

--- lab-daniel/test/greet.test.js
+++ lab-daniel/test/greet.test.js
@@ -4,11 +4,15 @@ const greet = require('../lib/greet.js').greet;
   describe('greet function', () => {
     test('it should return null when input is not a string', () => {
         let str = 9;
+        let result = greet(str);
         let expected = null;
+        expect(result).toBe(expected);
     });
     test('it should return string when input is a string', () => {
-      let str = 'hello world';
+      let str = 'world'
+      let result = 'hello' + str;
       let expected = true;
+      expect(result).toBe(expected);
     });
   });
 
