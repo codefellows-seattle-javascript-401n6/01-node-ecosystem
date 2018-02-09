5b2cf89e93c782a8cfbeea3cc26be1184334fbf9
Brandon Buchholz <blue.buchholz@gmail.com>
2018-02-08 21:03:15 -0800

    completed array-util.js file, also refactored the arithmetic.js file. Passes all test.

--- lab-brandon/test/arithmetic.test.js
+++ lab-brandon/test/arithmetic.test.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const addAll = require('../lib/arithmetic.js').addAll 
-const subAll = require('../lib/arithmetic.js').subAll 
+ const Arithmetic = require('../lib/arithmetic.js').Arithmetic;
 
-//arithmetic test for adding
+// test for adding
 describe("arithmetic function for adding", () => {  //describe what you’re testing
     describe("Function will return null if either parameter is not a number", () => {  
         let expected = null
     it("If either number is not a number it returns null", () => {
         let num1 ="not a number";
         let num2 = "not a number";
-        let result = addAll(num1, num2)
+        let result = Arithmetic.addAll(num1, num2)
         expect(result).toBe(expected);
         })
     })
@@ -20,18 +19,18 @@ describe("arithmetic function for adding", () => {  //describe what you’re tes
     it("It should add up num1 and num2 if both numbers", () => {
         let num1 = 5;
         let num2 = 10;
-        let result = addAll(num1,  num2);
+        let result = Arithmetic.addAll(num1,  num2);
         expect(result).toBe(expected);
     })
 })
-//arithmetic test for subtraction
+//test for subtraction
 describe("arithmetic function for subtraction", () => {  //describe what you’re testing
     describe("Function will return null if either parameter is not a number", () => {  
         let expected = null
     it("If either number is not a number it returns null", () => {
         let num1 ="not a number";
         let num2 = "not a number";
-        let result = subAll(num1, num2)
+        let result = Arithmetic.subAll(num1, num2)
         expect(result).toBe(expected);
         })
     })
@@ -41,7 +40,7 @@ describe("arithmetic function for subtraction", () => {  //describe what you’r
     it("It will subtract num2 from num1 if both parameters are numbers", () => {
         let num1 = 5;
         let num2 = 10;
-        let result = subAll(num1, num2);
+        let result = Arithmetic.subAll(num1, num2);
         expect(result).toBe(expected);
     })
 })
