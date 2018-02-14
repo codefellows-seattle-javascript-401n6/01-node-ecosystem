c672e4d0e1656626596f3b2527ba066e89ccec81
rjtj2007 <teacherman1@gmail.com>
2018-02-13 17:25:00 -0800

    still working

--- lab-ryan/lib/array-util.js
+++ lab-ryan/lib/array-util.js
@@ -1,1 +1,30 @@
 "use strict";
+
+function ArrayUtil(arr){
+
+    function sumArr(arr){
+        if(arr.length === 0){
+            return 0;
+        }
+    var sum = arr.reduce(function (a, b){
+        return a+ b;
+    }, 0);
+}
+
+    function minArr(arr){
+        if(arr.length === 0){
+
+        }
+    }
+
+    function isSorted(arr){
+        
+    }
+}
+
+
+
+module.exports = {};
+module.exports.ArrayUtil = ArrayUtil;
+module.exports.minArr = minArr;
+module.exports.isSorted = isSorted;
