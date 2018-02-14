bbecb4f7ca2802dca828c881998ad13a4805713b
rjtj2007 <teacherman1@gmail.com>
2018-02-13 20:29:36 -0800

    workijng on tests for arithmetic and array files

--- lab-ryan/test/arithmetic-test.js
+++ lab-ryan/test/arithmetic-test.js
@@ -1,19 +1,70 @@
+"use strict";
 // arithmetic test
 const add = require('../lib/arithmetic.js').add;
 const sub = require('../lib/arithmetic.js').sub;
 
-describe("add function", () => {
-    it("it should add two numbers and return a sum", () => {
-        expect(add(1, 2)).toBe(3);
-    });
-    it("it should add two numbers and return the sum", () => {
-        expect(add(a, b)).toBe(c);
-    });
-    it("it should subtract two numbers and return the result", () => {
-        expect(sub(6, 3)).toBe(3);
-    });
-    it("it should subtract two values and return the result", () => {
-        expect(sub(a, b)).toBe(c);
-    });
-    
+/////////////////
+
+describe('add function', () => {
+  it('should return null if x or y is not a  number', () => {
+    let x = NaN;
+    let y = 1;
+    let result = null;
+    let expected = null;
+    expect(result).toBe(expected);
+  });
+
+  it('should return null if x or y is undefined', () => {
+    let x = undefined;
+    let y = 1;
+    let result = null;
+    let expected = null;
+    expect(result).toBe(expected);
+  });
+
+  it('should return null if either x or y is a string value', () => {
+    let x = 1;
+    let y = 'foo';
+    let result = null;
+    let expected = null;
+    expect(result).toBe(expected);
+  });
+
+  it('should return the sum if x and y are both numbers', () => {
+    let x = 12;
+    let y = 8;
+    let result = add(x, y);
+    let expected = x + y;
+    expect(result).toBe(expected);
+  });
+});
+
+
+//sub tests
+describe('subtract function', () => {
+  it('should return null if x or y is not a  number', () => {
+    let x = NaN;
+    let y = 1;
+    let result = null;
+    let expected = null;
+    expect(result).toBe(expected);
+  });
+
+   //write test for negative numbers
+
+  it('should return null if x or y is a string value', () => {
+    let x = 1;
+    let y = 'foo';
+    let result = null;
+    let expected = null;
+    expect(result).toBe(expected);
+  });
+
+  it('should return the sub if x and y are both numbers', () => {
+    let x = 12;
+    let y = 8;
+    let result = subtract(x, y);
+    let expected = x - y;
+    expect(result).toBe(expected);
+  });
 });
--- lab-ryan/test/array-util-test.js
+++ lab-ryan/test/array-util-test.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const ArrayUtils = require('../lib/array-util-test.js');
+const ArrayUtil = require('../lib/array-util-test.js');
+const ArrayUtil = require('../path/to/arrayu-util.js');
+let sum = ArrayUtil.sumArr([1, 2, 3, 4, 5])
+let min = ArrayUtil.minArr([1, 2, 3, 4, 5])
+let isSorted = ArrayUtil.isSorted([1,2,3,4,5])
+
 
 describe("we should be able to access exported functions", () => {
     it("each function should be accessable", () => {
-        let sum = ArrayUtils.sumArr([1,2,3,4,5]);
-        let min = ArrayUtils.minArr([1,2,3,4,5]);
-        let isSorted = ArrayUtils.isSorted([1,2,3,4,5]);
+        let sum = ArrayUtil.sumArr([1,2,3,4,5]);
+        let min = ArrayUtil.minArr([1,2,3,4,5]);
+        let isSorted = ArrayUtil.isSorted([1,2,3,4,5]);
         // let notSorted = ArrayUtils.notSorted([1,2,3,4,5]); 
     
         expect(sum).toBe(15);
@@ -15,3 +20,94 @@ describe("we should be able to access exported functions", () => {
         // expect(notSorted).toBe(false);
     })
 })
+
+///////////////////////////////////////////////////
+const sum = require('../lib/array-util.js').sum;
+const min = require('../lib/array-util.js').min;
+const isSorted = require('../lib/array-util.js').isSorted;
+
+describe('sum function', () => {
+  test('should return 0 if the array is empty', () => {
+    let arr = [];
+    let result = sum(arr);
+    let expected = 0;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return a sum of all numbers in array is they are all positive', () => {
+    let arr = [1, 2, 4, 3];
+    let result = sum(arr);
+    let expected = 10;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return with the sum of all elements even if there are negative numbers in the array', () => {
+    let arr = [1, -2, 4, -5];
+    let result = sum(arr);
+    let expected = -2;
+    expect(result).toBe(expected);
+  });
+});
+
+
+describe('min function', () => {
+    //needs fixing
+  test('should return undefined if the array is empty', () => {
+    let arr = [];
+    let result = min(arr);//ends up returning Infinity instead of undefined
+    let expected = undefined;
+    expect(result).toBe(expected);
+  });
+
+  test('should return the smallest number when the array only has one value', () => {
+    let arr = [5];
+    let result = min(arr);
+    let expected = 5;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return the smallest number in a small collection of numbers', () => {
+    let arr = [1, 2, 4, 3];
+    let result = min(arr);
+    let expected = 1;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return the smallest number when there\'s a collection of both positive and negative numbers', () => {
+    let arr = [1, -2, 4, -5];
+    let result = min(arr);
+    let expected = -5;
+    expect(result).toBe(expected);
+  });
+});
+
+
+describe('isSorted function', () => {
+  test('should return true if the array is empty', () => {
+    let arr = [];
+    let result = isSorted(arr);
+    let expected = true;
+    expect(result).toBe(expected);
+  });
+
+  test('should return true when the array only has one value', () => {
+    let arr = [5];
+    let result = isSorted(arr);
+    let expected = true;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return true for a sorted array', () => {
+    let arr = [1, 2, 11, 13];
+    let result = isSorted(arr);
+    let expected = true;
+    expect(result).toBe(expected);
+  });
+  
+  it('should return false for an unsorted array', () => {
+    let arr = [1, -2, 4, -5];
+    let result = isSorted(arr);
+    let expected = false;
+    expect(result).toBe(expected);
+  });
+});
