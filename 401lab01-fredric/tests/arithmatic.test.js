40628f75608fb1300eba0e13feca0812ae7414fc
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2018-02-13 19:47:21 -0800

    grr forgot to uncomment out the string test. But it works!

--- 401lab01-fredric/tests/arithmatic.test.js
+++ 401lab01-fredric/tests/arithmatic.test.js
@@ -6,7 +6,7 @@ describe("This tests all the functions of the arithmetic.js file", () => {
   describe("this tests the add function", () => {
     test("math.add will return the sum of two numbers", () => {
       expect(arithmetic.add(3, 6)).toEqual(9);
-      //expect(math.add('cowmoo', 'moocow')).toEqual(null);
+      expect(arithmetic.add('cowmoo', 'moocow')).toEqual(null);
     });
   });
 });
