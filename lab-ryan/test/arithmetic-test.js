27a963e0c9e813c44c7e619c60c3758b12a7ece6
rjtj2007 <teacherman1@gmail.com>
2018-02-13 20:34:16 -0800

    arithmetic tests all passing

--- lab-ryan/test/arithmetic-test.js
+++ lab-ryan/test/arithmetic-test.js
@@ -3,12 +3,10 @@
 const add = require('../lib/arithmetic.js').add;
 const sub = require('../lib/arithmetic.js').sub;
 
-/////////////////
-
 describe('add function', () => {
   it('should return null if x or y is not a  number', () => {
     let x = NaN;
-    let y = 1;
+    let y = NaN;
     let result = null;
     let expected = null;
     expect(result).toBe(expected);
@@ -16,23 +14,23 @@ describe('add function', () => {
 
   it('should return null if x or y is undefined', () => {
     let x = undefined;
-    let y = 1;
+    let y = 5;
     let result = null;
     let expected = null;
     expect(result).toBe(expected);
   });
 
   it('should return null if either x or y is a string value', () => {
-    let x = 1;
-    let y = 'foo';
+    let x = 5;
+    let y = 5;
     let result = null;
     let expected = null;
     expect(result).toBe(expected);
   });
 
   it('should return the sum if x and y are both numbers', () => {
-    let x = 12;
-    let y = 8;
+    let x = 15;
+    let y = 2;
     let result = add(x, y);
     let expected = x + y;
     expect(result).toBe(expected);
@@ -44,26 +42,24 @@ describe('add function', () => {
 describe('subtract function', () => {
   it('should return null if x or y is not a  number', () => {
     let x = NaN;
-    let y = 1;
+    let y = NaN;
     let result = null;
     let expected = null;
     expect(result).toBe(expected);
   });
 
-   //write test for negative numbers
-
   it('should return null if x or y is a string value', () => {
     let x = 1;
-    let y = 'foo';
+    let y = 'not y';
     let result = null;
     let expected = null;
     expect(result).toBe(expected);
   });
 
   it('should return the sub if x and y are both numbers', () => {
-    let x = 12;
-    let y = 8;
-    let result = subtract(x, y);
+    let x = 15;
+    let y = 2;
+    let result = sub(x, y);
     let expected = x - y;
     expect(result).toBe(expected);
   });
