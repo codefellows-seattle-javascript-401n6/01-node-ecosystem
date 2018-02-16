bad1417a619610c5189d9f807ba9d2c9479a8b63
rjtj2007 <teacherman1@gmail.com>
2018-02-15 22:29:27 -0800

    all but one test passing, working on readme.md and final test

---
+++ lab-ryan/README.md
@@ -0,0 +1,16 @@
+#Lab-01 Node Ecosystem
+##Ryan Johnson
+
+This lab introduced the JEST testing framework for writing test driven development (TDD) programs. 
+
+##Module
+
+##arithmetic.js
+
+##array-util.js
+
+##greet.js
+
+
+
+
--- lab-ryan/lib/array-util.js
+++ lab-ryan/lib/array-util.js
@@ -4,7 +4,7 @@ function sum(arr) {
     if (arr.length === 0) {
         return 0;
     }
-    var sum = arr.reduce(function (a, b) {
+    let sum = arr.reduce(function (a, b) {
         return a + b;
     }, 0);
     return sum;
@@ -14,14 +14,34 @@ function min(arr) {
     if (arr.length === 0) {
         return undefined;
     }
-    return Math.min(arr);
+    let small = arr[0];
+    for (let i = 1; i < arr.lenght; i++){
+        if (arr[i] < small){
+            small = arr[i];
+        }
+    }
+    return small;
 }
 
 function isSorted(arr) {
-    if(arr.length === 0){
-        return true;
+    let isSorted;
+    if(arr[0] < arr[arr.length-1]){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] >  arr[i + 1]){
+                return false;
+            }
+        }
     }
+    if(arr[0] > arr[arr.length-1]){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] < arr[i + 1]){
+                return false;
+            }
+        }
+    }
+    return true;
 }
+
 
 module.exports = {};
 module.exports.sum = sum;
