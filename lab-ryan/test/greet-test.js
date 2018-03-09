aaf8758b8a014083b574f48634d8b9247aa6f9d1
rjtj2007 <teacherman1@gmail.com>
2018-03-08 17:43:33 -0800

    greet tests working properly

--- lab-ryan/test/greet-test.js
+++ lab-ryan/test/greet-test.js
@@ -1,25 +1,32 @@
-"use strict";
+'use strict';
 
 // greet test
 const greet = require('../lib/greet.js').greet;
 
-describe("greet function", () => {
-    it("it should return null with non-string value", () => {
-        let str = 1;
-        let result = greet(str);
-        let expected = null;
-        expect(result).toBe(expected); 
-    });
-    it("it should return 'hello world'", () => {
-        let str = 'world';
-        let result = greet(str);
-        let expected = 'hello world';
-        expect(result).toBe(expected); 
-    });
-    it("it should return null if input is undefinede",() => {
-        let str = undefined;
-        let result = greet(str);
-        let expected =  null;
-        expect(result).toBe(expected); 
-    });
+
+describe('greet function ', () => {
+  it('it should return null for an empty string',() => {
+    let str = '';
+    let result = greet(str);
+    let expected = null;
+    expect(result).toBe(expected);
+  })
+});
+
+describe('greet function ', () => {
+    it('it should return true for any non empty string',() => {
+      let str = 'dude';
+      let result = greet(str);
+      let expected = 'hello ' + 'dude';
+      expect(result).toBe(expected);
+    })
+  });
+
+describe('greet function ', () => {
+  it('it should return hello world when world is passed in',() => {
+    let str = 'world';
+    let result = greet(str);
+    let expected = 'hello ' + 'world';
+    expect(result).toBe(expected);
+  })
 });
